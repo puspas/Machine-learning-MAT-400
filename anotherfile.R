0faaf5d01ee669170afcd77c12a869f705370b75
puspas <puspanjali.subudhi@maine.edu>
2019-02-07 15:31:36 -0500

    optimized the code

---
+++ anotherfile.R
@@ -0,0 +1,14 @@
+
+7:2
+seq(2,14,3)
+rep(2,7)
+matrix(3,2,3)
+add = function(x,y) x+y
+add(21,23)
+
+f=function(x,y,z) {a = x*y+z;b=2*sin(x-z) 
+return(list(a,b))
+}
+f(2,8,12)
+
+plot(a,b,type='l')
